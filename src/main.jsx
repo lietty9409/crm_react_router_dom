a1e52891ea288ec66e4fe003df23c09cf3d62a1e
lietty9409 <141772799+lietty9409@users.noreply.github.com>
2023-08-29 23:00:05 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente,{action as nuevoClienteAction} from './pages/NuevoCliente'
+import './index.css'
+import Index,{loader as clientesLoader} from './pages/Index'
+import ErrorPage from './components/ErrorPage'
+import EditarCliente,{loader as editarClienteLoader, action as editarClienteAction} from './pages/EditarCliente'
+import {action as eliminarClienteAction} from './components/Clientes'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Layout/>,
+    children: [
+      {
+        index:true,
+        element:<Index/>,
+        loader: clientesLoader,
+        errorElement: <ErrorPage/>
+      },
+      {
+        path:'/clientes/nuevo',
+        element:<NuevoCliente/>,
+        action: nuevoClienteAction
+      },
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente/>,
+        loader: editarClienteLoader,
+        errorElement: <ErrorPage/>,
+        action: editarClienteAction
+      },
+      {
+        path: '/clientes/:clienteId/eliminar',
+        action: eliminarClienteAction
+      }
+    ]
+  }
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/Index.jsx
@@ -0,0 +1,46 @@
+import { useLoaderData } from "react-router-dom";
+import Clientes from "../components/Clientes";
+import { obtenerClientes } from "../data/clientes";
+
+export function loader() {
+  const clientes = obtenerClientes()
+  return clientes
+}
+
+const Index = () => {
+
+  const clientes = useLoaderData();
+
+
+  return (
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Clientes</h1>
+      <p className='mt-3'>Administra tus clientes</p>
+
+      {clientes.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Clientes</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+              {clientes.map( cliente => (
+                <Clientes 
+                  cliente = {cliente}
+                  key={cliente.id}
+                />
+              ))}
+          </tbody>
+          
+        </table>
+      ) : (
+        <p>No hay clientes aún</p>
+      )}
+    </>
+  )
+}
+
+export default Index
